c1eb46b13a8aba0128ccf3ace4e3ae0d40d143a1
danazou <dana.sou@gmail.com>
2022-09-21 23:32:10 -0700

    completed with bonus and ajdusted horizon height

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function tile(url, left, bottom, width, height){
-    for(let h = 0; h < height; h++){
-        for(let w = 0; w < width; w++){
-            newImage(url, left + w*100, bottom+h*100)
-        }
-    }
-}
+// using object as label for each image
 
 function newImage(url, left, bottom){
     let object = document.createElement('img')
@@ -24,13 +18,20 @@ function newItem(url, left, bottom){
     })
 }
 
-let horizon = window.innerHeight / 1.75
+function tile(url, left, bottom, width, height){
+    for(let h = 0; h < height; h++){
+        for(let w = 0; w < width; w++){
+            newImage(url, left + w*100, bottom+h*100)
+        }
+    }
+}
+
+let horizon = window.innerHeight / 3.0
 let heightOfSky = window.innerHeight-horizon
 let heightOfGrass = horizon
 
 tile('assets/sky.png', 0, horizon, window.innerWidth/100, heightOfSky/100)
 tile('assets/grass.png', 0, 0, window.innerWidth/100, heightOfGrass/100)
-
 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
@@ -40,7 +41,22 @@ newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
 newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
+newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
 
 
+/*let greenCharacter = document.createElement('img')
+greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '100px'
+greenCharacter.style.bottom = '100px'
+document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
+
+above is starter code that would have to be repeated to add each item */
